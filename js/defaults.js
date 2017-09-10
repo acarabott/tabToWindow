39b6cc439d0b755a3c1776530b2fabf504d14290
Arthur Carabott <arthurc@gmail.com>
2017-09-10 23:41:59 +0100

    ensure defaults are set in localStorage

---
+++ js/defaults.js
@@ -0,0 +1,31 @@
+(() => {
+  const localStorageDefaults = {
+    // 'original' or 'new'
+    'ttw_focus': 'new',
+    // 'true' or 'false'
+    'ttw_resize_original': 'true',
+    // 'true' or 'false'
+    'ttw_clone_original': 'false',
+    // 'clone-position-same', 'clone-position-horizontal', 'clone-position-vertical'
+    'ttw_clone_position': 'clone-position-horizontal',
+    // 'true' or 'false'
+    'ttw_copy_fullscreen': 'true',
+    // TODO this is left undefined for now, as calculated
+    // 'ttw_min_top': '',
+    // TODO these are redundant, should be consolidated
+    'ttw_original-width': 50,
+    'ttw_original-height': 100,
+    'ttw_original-left': 0,
+    'ttw_original-top': 0,
+    'ttw_new-width': 50,
+    'ttw_new-height': 100,
+    'ttw_new-left': 50,
+    'ttw_new-top': 0
+  };
+
+  Object.keys(localStorageDefaults).forEach(key => {
+    if (!localStorage.hasOwnProperty(key)) {
+      localStorage[key] = localStorageDefaults[key];
+    }
+  });
+})();
