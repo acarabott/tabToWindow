dbf81c4b53b65ffca5be63365fa7d3f3822ae259
Arthur Carabott <arthurc@gmail.com>
2018-03-05 12:55:47 +0000

    add onInstall hook

--- js/ttw.js
+++ js/ttw.js
@@ -381,6 +381,17 @@ chrome.commands.onCommand.addListener(command => {
   }
 });
 
+chrome.runtime.onInstalled.addListener(details => {
+  const previousMajorVersion = parseInt(details.previousVersion, 10);
+  const showUpdate = details.reason === "install" ||
+                     (details.reason === "update" && previousMajorVersion < 3);
+
+  if (showUpdate) {
+    const url = "https://acarabott.github.io/tabToWindow";
+    chrome.tabs.create({ url, active: true });
+  }
+});
+
 // Extension Button
 // -----------------------------------------------------------------------------
 
