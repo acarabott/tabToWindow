bc74610d53500d247fcaab3fd71e4601f379f761
Arthur Carabott <arthurc@gmail.com>
2020-02-09 12:03:39 -0800

    ts: move types from options-storage to api

--- ts/api.ts
+++ ts/api.ts
@@ -1,6 +1,26 @@
+export type WindowID = "original" | "new";
+export const windowProperties = ["width", "height", "left", "top"] as const;
+export type WindowProperty = typeof windowProperties[any];
+
 export interface IBounds {
   left: number;
   top: number;
   width: number;
   height: number;
+}
+
+export interface IOptions {
+  focus: WindowID;
+  resizeOriginal: boolean;
+  cloneMode: "clone-mode-no" | "clone-mode-same" | "clone-mode-horizontal" | "clone-mode-vertical";
+  copyFullscreen: boolean;
+  menuButtonType: "normal" | "popup";
+  originalWidth: number;
+  originalHeight: number;
+  originalLeft: number;
+  originalTop: number;
+  newWidth: number;
+  newHeight: number;
+  newLeft: number;
+  newTop: number;
 }
--- ts/options-storage.ts
+++ ts/options-storage.ts
@@ -1,24 +1,4 @@
-/* global chrome */
-
-export type WindowID = "original" | "new";
-export const windowProperties = ["width", "height", "left", "top"] as const;
-export type WindowProperty = typeof windowProperties[any];
-
-export interface IOptions {
-  focus: WindowID;
-  resizeOriginal: boolean;
-  cloneMode: "clone-mode-no" | "clone-mode-same" | "clone-mode-horizontal" | "clone-mode-vertical";
-  copyFullscreen: boolean;
-  menuButtonType: "normal" | "popup";
-  originalWidth: number;
-  originalHeight: number;
-  originalLeft: number;
-  originalTop: number;
-  newWidth: number;
-  newHeight: number;
-  newLeft: number;
-  newTop: number;
-}
+import { IOptions, WindowID, WindowProperty } from "./api";
 
 const defaults: IOptions = {
   focus: "new",
