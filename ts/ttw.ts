25b568d56eaa98644552912b69856b4c4b888371
Arthur Carabott <arthurc@gmail.com>
2020-02-09 23:59:11 -0800

    defaults

--- ts/ttw.ts
+++ ts/ttw.ts
@@ -55,7 +55,12 @@ getOptions().then(options => {
   };
 
   const getWindowBounds = (win: chrome.windows.Window): IBounds => {
-    return { left: win.left!, top: win.top!, width: win.width!, height: win.height! };
+    return {
+      left: win.left ?? 0,
+      top: win.top ?? 0,
+      width: win.width ?? screen.availWidth,
+      height: win.height ?? screen.availHeight,
+    };
   };
 
   const getNewWindowBounds = async (origWindow: chrome.windows.Window, displayBounds: IBounds) => {
