ea1fd1b526b0e0e19d54761f5e6c2713e2c31322
Arthur Carabott <arthurc@gmail.com>
2020-02-09 19:17:37 -0800

    ts: convert clone.ts functions to arrows

--- ts/clone.ts
+++ ts/clone.ts
@@ -1,54 +1,59 @@
 import { IBounds, IOptions } from "./api";
 
-function getPosAndLength(
-  winPos: number,
-  winLength: number,
-  displayPos: number,
-  displayLength: number,
-) {
-  const normWinPos = winPos - displayPos;
-  const oppositeEdge = normWinPos + winLength;
-  const oppositeGap = displayLength - oppositeEdge;
-  const doesFit = winLength <= oppositeGap;
-  const useOppositeGap = doesFit || normWinPos < oppositeGap;
-
-  const pos = useOppositeGap ? displayPos + oppositeEdge : winPos - Math.min(winLength, normWinPos);
-
-  const length = Math.min(winLength, useOppositeGap ? oppositeGap : normWinPos);
-
-  return { pos, length };
-}
-
-function getHorzPosAndLength(winBounds: IBounds, displayBounds: IBounds) {
-  return getPosAndLength(winBounds.left, winBounds.width, displayBounds.left, displayBounds.width);
-}
-
-function getVertPosAndLength(winBounds: IBounds, displayBounds: IBounds) {
-  return getPosAndLength(winBounds.top, winBounds.height, displayBounds.top, displayBounds.height);
-}
-
 // find the position that has the most space and return the position
 // and length to fill it.
 // e.g. when cloning horizontally and the window is left: 25% width: 25%
 // there is more space on the right side than the left, so use the right
 // pos is left/top opposite is right/bottom
 
-export function getCloneBounds(
+export const getCloneBounds = (
   winBounds: IBounds,
   displayBounds: IBounds,
   cloneMode: IOptions["cloneMode"],
-) {
+) => {
+  const getPosAndLength = (
+    winPos: number,
+    winLength: number,
+    displayPos: number,
+    displayLength: number,
+  ) => {
+    const normWinPos = winPos - displayPos;
+    const oppositeEdge = normWinPos + winLength;
+    const oppositeGap = displayLength - oppositeEdge;
+    const doesFit = winLength <= oppositeGap;
+    const useOppositeGap = doesFit || normWinPos < oppositeGap;
+
+    const pos = useOppositeGap
+      ? displayPos + oppositeEdge
+      : winPos - Math.min(winLength, normWinPos);
+
+    const length = Math.min(winLength, useOppositeGap ? oppositeGap : normWinPos);
+
+    return { pos, length };
+  };
+
   // copying all values covers the case of clone-mode-same
-  const bounds = Object.assign({}, winBounds);
+  const bounds = { ...winBounds };
   if (cloneMode === "clone-mode-horizontal") {
-    const { pos, length } = getHorzPosAndLength(bounds, displayBounds);
+    const { pos, length } = getPosAndLength(
+      winBounds.left,
+      winBounds.width,
+      displayBounds.left,
+      displayBounds.width,
+    );
     bounds.left = pos;
     bounds.width = length;
   } else if (cloneMode === "clone-mode-vertical") {
-    const { pos, length } = getVertPosAndLength(bounds, displayBounds);
+    const { pos, length } = getPosAndLength(
+      winBounds.top,
+      winBounds.height,
+      displayBounds.top,
+      displayBounds.height,
+    );
+
     bounds.top = pos;
     bounds.height = length;
   }
 
   return bounds;
-}
+};
