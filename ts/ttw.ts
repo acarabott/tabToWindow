7d2f055bb10e8651e40ad6aa66eca8734b75c142
Arthur Carabott <arthurc@gmail.com>
2020-02-09 23:41:16 -0800

    tidy up tabToNextWindow

--- ts/ttw.ts
+++ ts/ttw.ts
@@ -307,48 +307,43 @@ getOptions().then(options => {
   const tabToWindowPopup = () => tabToWindow("popup");
   const tabToNextDisplay = () => tabToWindow(undefined, true);
   const tabToNextWindow = async () => {
-    const tabsToMove = await new Promise<chrome.tabs.Tab[]>(resolve => {
-      chrome.tabs.query(
-        {
-          currentWindow: true,
-          highlighted: true,
-        },
-        tabs => {
-          if (tabs.length > 0) {
-            resolve(tabs);
-          }
-        },
-      );
-    });
+    const tabsToMove = await new Promise<chrome.tabs.Tab[]>(resolve =>
+      chrome.tabs.query({ currentWindow: true, highlighted: true }, tabs => resolve(tabs)),
+    );
+
+    if (tabsToMove.length === 0) {
+      return;
+    }
 
     const nextWindowId = await getNextWindowId(tabsToMove[0].windowId);
     if (nextWindowId === undefined) {
       return;
     }
 
+    // focus on next window
     await new Promise(resolve => {
       chrome.windows.update(nextWindowId, { focused: true }, win => resolve(win));
     });
 
+    // store tabs to unhighlight
     const tabsToUnhighlight = await getTabsToUnhighlight(nextWindowId);
+
+    // move and highlight selected tabs
     const moveIndex = -1;
     const movedTabs = await moveTabs(tabsToMove, nextWindowId, moveIndex);
+    await Promise.all(
+      movedTabs.map((tab, i) => {
+        return new Promise(tabResolve => {
+          chrome.tabs.update(
+            tab.id!,
+            { highlighted: true, active: i === movedTabs.length - 1 },
+            tab => tabResolve(tab),
+          );
+        });
+      }),
+    );
 
-    const highlightTabPromises = movedTabs.map((tab, i) => {
-      return new Promise(tabResolve => {
-        chrome.tabs.update(
-          tab.id!,
-          {
-            highlighted: true,
-            active: i === movedTabs.length - 1,
-          },
-          tab => tabResolve(tab),
-        );
-      });
-    });
-
-    await Promise.all(highlightTabPromises);
-
+    // unlight old tabs
     unhighlightTabs(tabsToUnhighlight);
   };
 
