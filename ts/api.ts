51d7407d25881e02f3223aa2f6a53d7bbef470a3
Arthur Carabott <arthurc@gmail.com>
2020-02-09 19:58:39 -0800

    ts: api blah

--- ts/api.ts
+++ ts/api.ts
@@ -1,13 +1,31 @@
-export type WindowID = "original" | "new";
+export const windowIds = ["original", "new"] as const;
+export type WindowID = typeof windowIds[any];
+
 export const windowProperties = ["width", "height", "left", "top"] as const;
 export type WindowProperty = typeof windowProperties[any];
 
-export type WindowType = "normal" | "popup";
-export type CloneMode =
-  | "clone-mode-no"
-  | "clone-mode-same"
-  | "clone-mode-horizontal"
-  | "clone-mode-vertical";
+export const windowTypes = ["normal", "popup"] as const;
+export type WindowType = typeof windowTypes[any];
+
+export const cloneModes = [
+  "clone-mode-no",
+  "clone-mode-same",
+  "clone-mode-horizontal",
+  "clone-mode-vertical",
+] as const;
+export type CloneMode = typeof cloneModes[any];
+
+export const storedWindowBounds = [
+  "originalWidth",
+  "originalHeight",
+  "originalLeft",
+  "originalTop",
+  "newWidth",
+  "newHeight",
+  "newLeft",
+  "newTop",
+] as const;
+export type StoredWindowProperty = typeof storedWindowBounds[any];
 
 export interface IBounds {
   left: number;
@@ -17,27 +35,38 @@ export interface IBounds {
 }
 
 export interface IOptions {
-  focus: WindowID;
-  resizeOriginal: boolean;
   cloneMode: CloneMode;
   copyFullscreen: boolean;
+  focus: WindowID;
   menuButtonType: WindowType;
-  originalWidth: number;
-  originalHeight: number;
-  originalLeft: number;
-  originalTop: number;
-  newWidth: number;
   newHeight: number;
   newLeft: number;
   newTop: number;
+  newWidth: number;
+  originalHeight: number;
+  originalLeft: number;
+  originalTop: number;
+  originalWidth: number;
+  resizeOriginal: boolean;
 }
 
-export type StoredWindowProperty =
-  | "originalWidth"
-  | "originalHeight"
-  | "originalLeft"
-  | "originalTop"
-  | "newWidth"
-  | "newHeight"
-  | "newLeft"
-  | "newTop";
+export const isIOptions = (obj: any): obj is IOptions => {
+  const cast = obj as IOptions;
+  return (
+    cast !== undefined &&
+    obj &&
+    cloneModes.includes(cast.cloneMode) &&
+    typeof cast.copyFullscreen === "boolean" &&
+    windowIds.includes(cast.focus) &&
+    windowTypes.includes(cast.menuButtonType) &&
+    typeof cast.newHeight === "number" &&
+    typeof cast.newLeft === "number" &&
+    typeof cast.newTop === "number" &&
+    typeof cast.newWidth === "number" &&
+    typeof cast.originalHeight === "number" &&
+    typeof cast.originalLeft === "number" &&
+    typeof cast.originalTop === "number" &&
+    typeof cast.originalWidth === "number" &&
+    typeof cast.resizeOriginal === "boolean"
+  );
+};
