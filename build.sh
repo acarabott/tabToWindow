d376f5f39c787f4ee12a69fd974b2ce099a4bef1
Arthur Carabott <arthurc@gmail.com>
2020-06-07 11:04:27 -0700

    update build script to build unpacked extension

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 BUILD_DIR="build";
 
-npm run build && \
+npx tsc -p . && \
 mkdir -p $BUILD_DIR && \
     cp -R background.html css js lib icons LICENSE manifest.json options.html $BUILD_DIR && \
     zip -r build.zip $BUILD_DIR
