daa4109332bcb3a326721a704172513c0f67c8de
Arthur Carabott <arthurc@gmail.com>
2020-07-12 17:51:04 -0700

    refactor getCloneBounds

--- ts/getCloneBounds.ts
+++ ts/getCloneBounds.ts
@@ -1,23 +1,31 @@
 import { CloneMode, IBounds } from "./api";
 
 /**
-  * find the position that has the most space and return the position
-  * and length to fill it.
-  * e.g. when cloning horizontally and the window is left: 25% width: 25%
-  * there is more space on the right side than the left, so use the right
-  * pos is left/top opposite is right/bottom
+ * find the position that has the most space and return the position
+ * and length to fill it.
+ * e.g. when cloning horizontally and the window is left: 25% width: 25%
+ * there is more space on the right side than the left, so use the right
+ * pos is left/top opposite is right/bottom
  */
 export const getCloneBounds = (
   winBounds: IBounds,
   displayBounds: IBounds,
   cloneMode: CloneMode,
 ) => {
-  const getPosAndLength = (
-    winPos: number,
-    winLength: number,
-    displayPos: number,
-    displayLength: number,
-  ) => {
+  // copying all values covers the case of clone-mode-same
+  const bounds = { ...winBounds };
+
+  if (cloneMode === "clone-mode-horizontal" || cloneMode === "clone-mode-vertical") {
+    const [posKey, lengthKey] = {
+      "clone-mode-horizontal": ["left", "width"] as const,
+      "clone-mode-vertical": ["top", "height"] as const,
+    }[cloneMode];
+
+    const winPos = winBounds[posKey];
+    const winLength = winBounds[lengthKey];
+    const displayPos = displayBounds[posKey];
+    const displayLength = displayBounds[lengthKey];
+
     const normWinPos = winPos - displayPos;
     const oppositeEdge = normWinPos + winLength;
     const oppositeGap = displayLength - oppositeEdge;
@@ -30,30 +38,8 @@ export const getCloneBounds = (
 
     const length = Math.min(winLength, useOppositeGap ? oppositeGap : normWinPos);
 
-    return { pos, length };
-  };
-
-  // copying all values covers the case of clone-mode-same
-  const bounds = { ...winBounds };
-  if (cloneMode === "clone-mode-horizontal") {
-    const { pos, length } = getPosAndLength(
-      winBounds.left,
-      winBounds.width,
-      displayBounds.left,
-      displayBounds.width,
-    );
-    bounds.left = pos;
-    bounds.width = length;
-  } else if (cloneMode === "clone-mode-vertical") {
-    const { pos, length } = getPosAndLength(
-      winBounds.top,
-      winBounds.height,
-      displayBounds.top,
-      displayBounds.height,
-    );
-
-    bounds.top = pos;
-    bounds.height = length;
+    bounds[posKey] = pos;
+    bounds[lengthKey] = length;
   }
 
   return bounds;
