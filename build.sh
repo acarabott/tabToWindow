c57b64580a41f78d8342c83a60e0644de24e40b9
Arthur Carabott <arthur@arthurcarabott.com>
2022-04-23 17:15:53 -0700

    re-organise source and build directory

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-BUILD_DIR="build";
+if [[ -z "${npm_package_version}" ]]; then
+    echo "could not detect version, must be run with \$npm run build"
+    exit 1
+fi
 
-npx tsc -p . && \
-mkdir -p $BUILD_DIR && \
-    cp -R background.html css js lib icons LICENSE manifest.json options.html $BUILD_DIR && \
-    zip -r build.zip $BUILD_DIR
+VERSION="$npm_package_version"
+
+BUILD_DIR="build" # this must match tsconfig.json:compilerOptions:outDir
+
+npx tsc -p . &&
+    mkdir -p "$BUILD_DIR" &&
+    cp -R src/background.html src/css src/icons src/lib src/manifest.json src/options.html "$BUILD_DIR" &&
+    zip -r "build-tab-to-window-$VERSION".zip "$BUILD_DIR"
