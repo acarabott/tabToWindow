faf02bd6299cea5797ed23c75782284bcb134011
unicornflax <94918393+unicornflax@users.noreply.github.com>
2024-04-12 12:41:03 +0200

    Update createNewWindow.ts

--- ts/createNewWindow.ts
+++ ts/createNewWindow.ts
@@ -4,9 +4,8 @@ export const createNewWindow = (
   tab: chrome.tabs.Tab,
   windowType: WindowType,
   windowBounds: IBounds,
-  isFullscreen: boolean,
-  isMaximized: boolean,
   isFocused: boolean,
+  state: chrome.windows.windowStateEnum | undefined,
 ): Promise<[chrome.windows.Window, chrome.tabs.Tab]> => {
   // new window options
   const opts: chrome.windows.CreateData = {
@@ -14,19 +13,13 @@ export const createNewWindow = (
     type: windowType,
     focused: isFocused,
     incognito: tab.incognito,
-    ...windowBounds,
+    ...(state === "normal" ? windowBounds : { state }),
   };
 
   return new Promise((resolve, reject) => {
     chrome.windows.create(opts, (newWin) => {
       if (newWin !== undefined) {
-        if (isFullscreen && newWin.id !== undefined) {
-          chrome.windows.update(newWin.id, { state: "fullscreen" }, () => resolve([newWin, tab]));
-        } else if (isMaximized && newWin.id !== undefined) {
-          chrome.windows.update(newWin.id, { state: "maximized" }, () => resolve([newWin, tab]));
-        } else {
-          resolve([newWin, tab]);
-        }
+        resolve([newWin, tab]);
       } else {
         reject("Could not create new window");
       }
