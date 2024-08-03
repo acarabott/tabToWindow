ce426ecfdaa23bd23fb79de14cb19839040fccfd
Arthur Carabott <arthur@arthurcarabott.com>
2024-08-03 12:12:28 -0700

    make watch script watch all files again

--- watch.sh
+++ watch.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-JS_DIR="js"
 BUILD_DIR="../build"
-
-mkdir -p $BUILD_DIR
-cp -R css lib icons LICENSE manifest.json options.html $BUILD_DIR
 
 echo "watching..."
 
 while true; do
-    rm -rf $JS_DIR &&
+    rm -rf $"BUILD_DIR/*" &&
+        mkdir -p $BUILD_DIR &&
         npx tsc -p . &&
-        rm -rf $BUILD_DIR/js &&
-        cp -R $JS_DIR $BUILD_DIR &&
-        echo "updated"
+        cp -R js css lib icons LICENSE manifest.json options.html $BUILD_DIR
+    echo "updated"
     sleep 1.5
 done
