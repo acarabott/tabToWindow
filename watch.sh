20fe568821b5624b4e70644ae237a0ab64ac7183
Arthur Carabott <arthur@arthurcarabott.com>
2024-08-03 11:09:38 -0700

    remove logging from watch script

--- watch.sh
+++ watch.sh
@@ -5,10 +5,11 @@ BUILD_DIR="../build"
 mkdir -p $BUILD_DIR
 cp -R css lib icons LICENSE manifest.json options.html $BUILD_DIR
 
+echo "watching..."
+
 while true; do
-    echo "building..." &&
-        npx tsc -p . &&
+    npx tsc -p . &&
         rm -rf $BUILD_DIR/js &&
         cp -R js $BUILD_DIR
-    sleep 1.5 
+    sleep 1.5
 done
