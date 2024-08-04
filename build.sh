4e0e947a6e960485e8df95aa5d9a669d58ec241e
Arthur Carabott <arthur@arthurcarabott.com>
2024-08-03 18:22:48 -0700

    add safety to build script

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@
 BUILD_DIR="../build"
 JS_DIR="js"
 
-rm -rf $JS_DIR &&
+mkdir -p $JS_DIR &&
+    rm -f "${JS_DIR:?}/"* &&
     npx tsc -p . &&
     mkdir -p $BUILD_DIR &&
-    rm -rf $"BUILD_DIR/*" &&
+    rm -rf "${BUILD_DIR:?}/"* &&
     cp -R css $JS_DIR lib icons LICENSE manifest.json options.html $BUILD_DIR
+echo "done"
